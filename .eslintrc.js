85d14de99550ec6a8ea61c597db134ac4949611e
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-10-08 18:52:29 +0530

    chore: update lint rules for overlap
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,11 +19,16 @@ module.exports = {
     'vue/singleline-html-element-content-newline': 0,
     'vue/html-self-closing': 0,
     'vue/html-closing-bracket-newline': 0,
+    'vue/html-indent': ['warn', 2, { baseIndent: 1 }],
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
   },
   globals: {
     $nuxt: true,
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
   },
-}
+};
